c806dc793fc7d6b2d1630a76c78d910f9fd21b85
Closarias <unax2015@gmail.com>
2025-12-18 11:00:59 +0100

    postal

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,9 +1,11 @@
 import {createBrowserRouter} from "react-router-dom";
 import PageNotFound from "@/pages/PageNotFound.tsx";
 import Header from "@/Componentes/Header.tsx";
+import Postales from "@/pages/Postales";
 
 
 export const router = createBrowserRouter([
     {path: "*", element: <PageNotFound/>},
-    {path: "/", element: <Header/>}
+    {path: "/", element: <Header/>},
+    {path: "/postales", element: <Postales/>}
 ]);
