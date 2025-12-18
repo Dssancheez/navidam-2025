d01a4fa9805ed17dcf518b246ba0df59435b96d9
Closarias <unax2015@gmail.com>
2025-12-18 14:04:51 +0100

    estoy harto del pull

--- frontend/src/pages/Personas.tsx
+++ frontend/src/pages/Personas.tsx
@@ -22,6 +22,7 @@ const [, setErrorMsg] = useState<string>();
 
 useEffect(()=>{;
         mostrarPersonas().then(response =>{
+            if(response.ok && response.data){
                 const personasObtenidas = response.data.map(a =>{
                     return{
                         id: a.id,
@@ -30,6 +31,9 @@ useEffect(()=>{;
                     }
                 });
                 setPersonas(personasObtenidas);
+            }else if(!response.ok){
+                console.log(response.error);
+            }
         }).catch((errorMsg:Error)=>{
             setErrorMsg(errorMsg.message);
         });
